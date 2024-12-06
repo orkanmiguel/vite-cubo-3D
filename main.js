30bc492063c39419081a1fce3f0e3d65197fdd75
orkanmiguel <orkan.miguel@gmail.com>
2024-12-06 01:11:05 -0300

    add texture test

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 import * as THREE from "three";
 
+const image = new Image();
+const texture = new THREE.Texture(image);
+
+image.onload = () => {
+  texture.needsUpdate = true;
+};
+
+image.src = "/static/captura.png";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -13,17 +22,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 3);
-const material = new THREE.MeshBasicMaterial({ color: "brown" });
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ map: texture });
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+for (var i = 0; i < 200; i++) {
+  scene.add(cube);
+}
+const light = new THREE.DirectionalLight(0xfffffff, 0.5);
+light.position.set(-2, 3, 1);
+scene.add(light);
+
+const helper = new THREE.DirectionalLightHelper(light, 1);
+scene.add(helper);
 
 camera.position.z = 5;
 
-import WebGL from "three/addons/capabilities/WebGL.js";
-
 if (WebGL.isWebGL2Available()) {
   // Initiate function or other initializations here
+
   animate();
 } else {
   const warning = WebGL.getWebGL2ErrorMessage();
@@ -35,6 +51,7 @@ function animate() {
   cube.rotation.y += 0.01;
   cube.position.x += 0.01;
   cube.position.y += 0.01;
+  cube.position.z += 0.01;
 
   if (cube.position.x > 3) {
     cube.position.x -= 0.01;
